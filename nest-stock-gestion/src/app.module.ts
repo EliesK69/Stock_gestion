608585ddde3abc8d93273e8e969356e5cdd8975e
EliesK69 <elies.kraimi@gmail.com>
2024-12-25 00:16:27 +0100

    création des modules via le CLI

--- nest-stock-gestion/src/app.module.ts
+++ nest-stock-gestion/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { ShopsModule } from './shops/shops.module';
+import { UsersModule } from './users/users.module';
 import { UsersModule } from './users/users.module';
 import { ShopsModule } from './shops/shops.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
