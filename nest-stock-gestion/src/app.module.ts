60257bc2ff25c97fbc788c38a9e535e058e4199d
EliesK69 <elies.kraimi@gmail.com>
2024-12-25 00:14:35 +0100

    suppression des dossiers manuellement créés

--- nest-stock-gestion/src/app.module.ts
+++ nest-stock-gestion/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ShopsModule } from './shops/shops.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/ecommerce'), // Connexion à MongoDB
+    AuthModule,
+    UsersModule,
+    ShopsModule,
+    ProductsModule,
+    OrdersModule,
+  ],
 })
 export class AppModule {}
