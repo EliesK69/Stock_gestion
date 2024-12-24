f8e99d724299d9efb3e0e4c2f96c670dcaa049c7
EliesK69 <elies.kraimi@gmail.com>
2024-12-25 00:17:53 +0100

    création des controllers et des services  via le CLI

--- nest-stock-gestion/src/shops/shops.module.ts
+++ nest-stock-gestion/src/shops/shops.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ShopsService } from './shops.service';
+import { ShopsController } from './shops.controller';
 
-@Module({})
+@Module({
+  providers: [ShopsService],
+  controllers: [ShopsController]
+})
 export class ShopsModule {}
